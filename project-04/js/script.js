096f0d4125e8f8bcdb1d674a2bc337194b7d0eeb
kabeer <kabeer.ahmed0120@gmail.com>
2023-08-28 05:36:01 +0500

    Loader built successfull

--- project-04/js/script.js
+++ project-04/js/script.js
@@ -1,3 +1,31 @@
+function time() {
+  var a = 0;
+  setInterval(function () {
+    a = a + Math.floor(Math.random() * 10);
+    if (a < 100) {
+      document.querySelector(".loader h1").innerHTML=a + "%";
+    }
+    else{
+      a=100;
+      document.querySelector(".loader h1").innerHTML=a + "%";
+    }
+  }, 100);
+}
+
+
+var tl = gsap.timeline();
+tl.to(".loader h1", {
+  delay: 0.5,
+  duration: 2,
+  onStart:time()
+});
+tl.to(".loader", {
+  top: "-200vh",
+  delay: 0.5,
+  duration: 1.5,
+  onStart:time()
+});
+
 gsap.to(".page1 h1", {
   transform: "translateX(-100%)",
   fontWeight:100,
@@ -10,7 +38,5 @@ gsap.to(".page1 h1", {
     scrub:3,
     pin:true,
   },
-
-
 
 });
